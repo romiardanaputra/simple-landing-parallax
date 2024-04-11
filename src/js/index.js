446bfd6ff5cd8ecab74aa29738209090e3c666e8
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-04-11 20:45:46 +0800

    Style/setup sass (#1)
    * style(scss): setup scss on new project
    
    * style(scss): setup scss on new project

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
-const a = alert('hello');
-
-const haloDunia = () => {
-  const a = 'hello';
-
-  return a;
-};
-
-haloDunia();
-
-console.log(a);
+class Button {
+  constructor(name, color, width, height, padding, margin) {
+    this.name = name;
+    this.color = color;
+    this.width = width;
+    this.height = height;
+    this.padding = padding;
+    this.margin = margin;
+  }
+}
+const SignInButton = new Button();
+console.log(SignInButton);
